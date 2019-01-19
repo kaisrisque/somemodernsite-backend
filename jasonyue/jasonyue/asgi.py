1a5329d90b8f941ed2f6270e87016b6f13e7de51
Jason Yue <kymotsujason@live.com>
2019-01-19 03:06:16 -0800

    Remove unnecessary code
    - Removed possibly unnecessary code

--- jasonyue/jasonyue/asgi.py
+++ jasonyue/jasonyue/asgi.py
@@ -8,5 +8,5 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jasonyue.production_settings")
-django.setup()
+
 application = get_default_application()
