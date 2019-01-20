2fefb8d3e374065095d9450dc6301bdd5728946a
Jason Yue <kymotsujason@live.com>
2019-01-20 01:32:45 -0800

    Some changes
    - Minor changes to url pathing

--- jasonyue/jasonyue/routing.py
+++ jasonyue/jasonyue/routing.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat import consumers
@@ -7,7 +7,7 @@ application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter([
-            url(r'^ws/chat/$', consumers.ChatConsumer ),
+            path('ws/chat', consumers.ChatConsumer),
         ])
     ),
 })
